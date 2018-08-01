ceb4f2079e4f898f7ff0bf3d7ae845101f5a6188
Hannah Wolfe <erisds@gmail.com>
2018-08-01 19:15:13 +0100

    Playing with typography

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography";
+
+//const typography = new Typography({ baseFontSize: "16px" });
+import theme from "typography-theme-funston";
+
+const typography = new Typography(theme);
+
+export default typography;
