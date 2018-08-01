48934ea461b7b7f16d48134af1f49f1b58dcca89
Hannah Wolfe <erisds@gmail.com>
2018-08-01 19:37:45 +0100

    Magical navigation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,27 @@
 import React from "react";
-
+import Link from "gatsby-link";
 import Container from "../components/container";
+
+const ListLink = props =>
+    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+        <Link to={props.to}>
+            {props.children}
+        </Link>
+    </li>
 
 export default ({ children }) => (
     <Container>
-        <h3>Testy McTestFace</h3>
+        <header style={{ marginBottom: `1.5rem` }}>
+            <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+                <h3 style={{ display: `inline` }}>Testy McTestFace</h3>
+            </Link>
+            <ul style={{ listStyle: `none`, float: `right` }}>
+                <ListLink to="/">Home</ListLink>
+                <ListLink to="/about/">About</ListLink>
+                <ListLink to="/contact/">Contact</ListLink>
+                <ListLink to="/counter/">Counter</ListLink>
+            </ul>
+        </header>
         {children()}
     </Container>
 );
