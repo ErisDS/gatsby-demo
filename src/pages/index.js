391235bf6865734cadb17d5a2358f418401ebbd7
Hannah Wolfe <erisds@gmail.com>
2018-08-01 18:16:10 +0100

    Following gatsby tut

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () =>
+    <div style={{ color: `tomato` }}>
+        <h1>Magic!</h1>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <br />
+        <div>
+            <Link to="/page-2/">Page 2</Link>
+        </div>
+        <div>
+            <Link to="/counter/">Counter</Link>
+        </div>
+    </div>
