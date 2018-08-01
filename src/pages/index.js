7132af191ff6b18a0e0af165e4532bff7cc391c0
Hannah Wolfe <erisds@gmail.com>
2018-08-01 19:38:33 +0100

    Reverting index.js content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,26 @@ import React from "react";
 
 export default () => (
     <div>
-        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-        <p>
-            What do I like to do? Lots of course but definitely enjoy building
-            websites.
-        </p>
+        <h1>Richard Hamming on Luck</h1>
+        <div>
+            <p>
+                From Richard Hamming’s classic and must-read talk, “<a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+                You and Your Research
+            </a>”.
+            </p>
+            <blockquote>
+                <p>
+                    There is indeed an element of luck, and no, there isn’t. The prepared
+                    mind sooner or later finds something important and does it. So yes, it
+                    is luck.{" "}
+                    <em>
+                        The particular thing you do is luck, but that you do something is
+                        not.
+                    </em>
+                </p>
+            </blockquote>
+        </div>
+        <p>Posted April 09, 2011</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
 );
