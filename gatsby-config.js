3d61500220d97791284e70324b059092453faab5
Hannah Wolfe <erisds@gmail.com>
2018-08-02 09:02:42 +0100

    Using source filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     },
     plugins: [
         {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        },
+        {
             resolve: `gatsby-plugin-typography`,
             options: {
                 pathToConfigModule: `src/utils/typography.js`,
